d293f6ae98aca2ee995f73adab044c44018c9c6b
math314 <314same@gmail.com>
2013-06-04 22:36:49 +0900

    refs #3 対応した

--- th125/client/hook_window.cpp
+++ th125/client/hook_window.cpp
@@ -8,9 +8,25 @@ const HWND *pMAIN_WND = (HWND *)0x004D17D8;
 static WNDPROC oldWndProc = NULL;
 HRESULT CALLBACK HookWindowProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam){
 	
-	// Send event message to AntTweakBar
-    if( TwEventWin(hWnd, msg, wParam, lParam) ){
-        return 0;   // Event has been handled by AntTweakBar
+	//AntTweakBarは、ウィンドウのサイズとdirectXのBackBufferサイズが違う場合に対応していない
+	//ので、此方側で対応する。
+	if(msg == WM_MOUSEMOVE){
+		RECT rc;
+		GetClientRect(hWnd,&rc);
+
+		int x = ((short)LOWORD(lParam)) * 640 / (rc.right - rc.left);
+		int y = ((short)HIWORD(lParam)) * 480 / (rc.bottom - rc.top);
+
+        // send signed! mouse coordinates
+        int handled = TwMouseMotion(x,y);
+		if(handled != 0){
+			return 0; //handled by AntTweakBar(TwMouseMotion)
+		}
+	} else {
+		// Send event message to AntTweakBar
+		if( TwEventWin(hWnd, msg, wParam, lParam) ){
+			return 0;   // Event has been handled by AntTweakBar
+		}
 	}
 
 	return oldWndProc(hWnd,msg,wParam,lParam);
